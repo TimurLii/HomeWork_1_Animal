5ac1780e183fd6c1230c13c3b3c771f48d977040
Timur_2101 <timurka260197@gmil.com>
2023-12-19 19:27:37 +0200

    Initial commit

---
+++ src/Crocodile.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public class Crocodile extends Animal {
+    private boolean fly ;
+    private boolean swim;
+    private boolean toGo;
+
+    public Crocodile(String name, LocalDate birthDay,
+                     List<String> vaccinations, String illness, String owner) {
+        super(name, birthDay, vaccinations, illness, owner);
+        this.fly = false;
+        this.swim = true;
+        this.toGo = true;
+
+    }
+    public boolean flying(){
+        return false;
+    }
+    public boolean swims(){
+        return true;
+    }
+    public boolean toGoing(){
+        return true;
+    }
+
+
+}
